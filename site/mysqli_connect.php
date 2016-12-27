c15adfdb2f60addffbd705354e4ac4c8e077f908
caglacinar <c.cinar@hotmail.com>
2016-12-28 00:19:29 +0200

    Değişiklikler
    Register - Login - Logout sayfaları eklendi. Mainpage.html sayfası
    Index.php olarak düzeltildi.

--- site/mysqli_connect.php
+++ site/mysqli_connect.php
@@ -3,13 +3,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "outingdb";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
+if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
 ?>
