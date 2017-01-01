b723240e579c71b680c93da71cbffd7711ac17c8
Serdar Ünlüsoy <serdarunlusoy96@gmail.com>
2017-01-01 16:35:42 +0300

    minor updates
    some column names have been changed
    an expressin should be written for availability column in hotels and price column in reservations in order to make them "generated" columns

--- database.sql
+++ database.sql
@@ -7,6 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -15,93 +16,94 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Users`
+-- Table `mydb`.`hotels`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Users` (
+DROP TABLE IF EXISTS `mydb`.`hotels` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`hotels` (
+  `RegistrationCode` VARCHAR(20) NOT NULL,
+  `HotelName` VARCHAR(45) NOT NULL,
+  `District` VARCHAR(20) NOT NULL,
+  `Phone` VARCHAR(20) NOT NULL,
+  `Stars` VARCHAR(1) NOT NULL,
+  `DailyPrice` INT(11) NOT NULL,
+  `Availabilty` VARCHAR(45) NOT NULL,
+  `Rooms` INT(11) NOT NULL,
+  `InfoPage` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`RegistrationCode`),
+  UNIQUE INDEX `Registration Code_UNIQUE` (`RegistrationCode` ASC),
+  UNIQUE INDEX `Info Page Address_UNIQUE` (`InfoPage` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`hotel users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`hotel users` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`hotel users` (
+  `HotelName` VARCHAR(45) NULL DEFAULT NULL,
+  `Hotels_RegistrationCode` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`Hotels_RegistrationCode`),
+  INDEX `fk_Hotel Users_Hotels1_idx` (`Hotels_RegistrationCode` ASC),
+  CONSTRAINT `fk_Hotel Users_Hotels1`
+    FOREIGN KEY (`Hotels_RegistrationCode`)
+    REFERENCES `mydb`.`hotels` (`RegistrationCode`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `userName` VARCHAR(10) CHARACTER SET 'big5' NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `password` VARCHAR(20) NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(20) NULL DEFAULT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `phone` VARCHAR(10) NULL,
+  `phone` VARCHAR(10) NULL DEFAULT NULL,
   `UserType` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`userName`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
   UNIQUE INDEX `userName_UNIQUE` (`userName` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Hotels`
+-- Table `mydb`.`reservations`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Hotels` (
-  `Registration Code` VARCHAR(20) NOT NULL,
-  `Hotel Name` VARCHAR(45) NOT NULL,
-  `District` VARCHAR(20) NOT NULL,
-  `Phone` VARCHAR(20) NOT NULL,
-  `Stars` VARCHAR(1) NOT NULL,
-  `Daily Price` INT NULL,
-  `Availabilty` VARCHAR(45) GENERATED ALWAYS AS () VIRTUAL,
-  `Rooms` INT NULL,
-  PRIMARY KEY (`Registration Code`),
-  UNIQUE INDEX `Registration Code_UNIQUE` (`Registration Code` ASC))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `mydb`.`reservations` ;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`Hotel Users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Hotel Users` (
-  `Hotel Name` VARCHAR(45) NULL,
-  `Hotels_Registration Code` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`Hotels_Registration Code`),
-  INDEX `fk_Hotel Users_Hotels1_idx` (`Hotels_Registration Code` ASC),
-  CONSTRAINT `fk_Hotel Users_Hotels1`
-    FOREIGN KEY (`Hotels_Registration Code`)
-    REFERENCES `mydb`.`Hotels` (`Registration Code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Reservations`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Reservations` (
-  `Reservation ID` INT NOT NULL AUTO_INCREMENT,
-  `Users_userName` VARCHAR(10) NOT NULL,
-  `Hotels_Registration Code` VARCHAR(20) NOT NULL,
-  `People` INT NOT NULL,
-  `Room` INT NOT NULL,
-  `Start Date` DATE NOT NULL,
-  `End Date` DATE NOT NULL,
-  `Price` INT GENERATED ALWAYS AS () VIRTUAL,
-  PRIMARY KEY (`Reservation ID`, `Users_userName`, `Hotels_Registration Code`),
-  INDEX `fk_Reservations_Users1_idx` (`Users_userName` ASC),
-  INDEX `fk_Reservations_Hotels1_idx` (`Hotels_Registration Code` ASC),
-  UNIQUE INDEX `Reservation ID_UNIQUE` (`Reservation ID` ASC),
+CREATE TABLE IF NOT EXISTS `mydb`.`reservations` (
+  `People` INT(11) NOT NULL,
+  `Room` INT(11) NOT NULL,
+  `StartDate` DATE NOT NULL,
+  `EndDate` DATE NOT NULL,
+  `Price` INT(11) NULL DEFAULT NULL,
+  `Users_userName` VARCHAR(10) CHARACTER SET 'big5' NOT NULL,
+  `Hotels_RegistrationCode` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`Users_userName`, `Hotels_RegistrationCode`),
+  INDEX `fk_Reservations_Hotels1_idx` (`Hotels_RegistrationCode` ASC),
+  CONSTRAINT `fk_Reservations_Hotels1`
+    FOREIGN KEY (`Hotels_RegistrationCode`)
+    REFERENCES `mydb`.`hotels` (`RegistrationCode`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Reservations_Users1`
     FOREIGN KEY (`Users_userName`)
-    REFERENCES `mydb`.`Users` (`userName`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Reservations_Hotels1`
-    FOREIGN KEY (`Hotels_Registration Code`)
-    REFERENCES `mydb`.`Hotels` (`Registration Code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`users` (`userName`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `mydb`.`Users`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `mydb`;
-INSERT INTO `mydb`.`Users` (`userName`, `name`, `password`, `email`, `phone`, `UserType`) VALUES ('admin', 'serdar', '123456789', 's@s.com', '123456789798798', 'admin');
-
-COMMIT;
-
