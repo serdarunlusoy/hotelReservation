ab6747cc1d42eab7e0fa7d7be8fcdcc760175493
caglacinar <c.cinar@hotmail.com>
2017-01-02 02:21:39 +0200

    Düzeltme
    login-logout-register

--- site/mysqli_connect.php
+++ site/mysqli_connect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mydb";
+$dbname = "hrsdb";
 
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
--- site/search.php
+++ site/search.php
@@ -1,93 +1,92 @@
 <?php
-  mysql_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
-  mysql_select_db("mydb") or die(mysql_error());
+error_reporting(E_ALL ^ E_DEPRECATED);
+include_once 'mysqli_connect.php';
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <title>HRS - results page</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <head>
+    <title>HRS - results page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-</head>
-<body>
+    </head>
+    <body>
 
- <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">HRS System</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Hotels</a></li>
-      <li><a href="#">About</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
-
-
-<div class="container">
-  <div class="row">
-
-    <div class="col-sm-4">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">HRS System</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Hotels</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </nav>
 
 
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4">
 
 
-      <div id="custom-search-input" style="padding-bottom:5px">
-        <div class="input-group col-md-12">
-          <input type="text" class="  search-query form-control" placeholder="Search" />
-          <span class="input-group-btn">
-            <button class="btn btn-primary" type="button">
-              <span class=" glyphicon glyphicon-search"></span>
-                </button>
-              </span>
-        </div>
-      </div>
 
 
-      <div class="panel panel-default">
-
-        <div class="panel-heading"> <button type="button" class="btn btn-primary btn-sm btn-block">Advanced Search</button> </div>
-        <div class="panel-body">
-          <ul class="list-group">
-              <li class="list-group-item">HERE</li>
-              <li class="list-group-item">BE</li>
-              <li class="list-group-item">ADVANCES</li>
-              <li class="list-group-item">OPTIONS</li>
-          </ul>
-
-        </div>
-      </div>
-
-    </div>
+                <div id="custom-search-input" style="padding-bottom:5px">
+                    <div class="input-group col-md-12">
+                        <input type="text" class="  search-query form-control" placeholder="Search" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" type="button">
+                                <span class=" glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
 
 
-<?php
-  $query = $_GET['query'];
+                <div class="panel panel-default">
 
-  $min_length = 3;
+                    <div class="panel-heading"> <button type="button" class="btn btn-primary btn-sm btn-block">Advanced Search</button> </div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">HERE</li>
+                            <li class="list-group-item">BE</li>
+                            <li class="list-group-item">ADVANCES</li>
+                            <li class="list-group-item">OPTIONS</li>
+                        </ul>
 
-  if(strlen($query) >= $min_length){ // if query length > minLen
-    $query = htmlspecialchars($query);
-    // html conversion
+                    </div>
+                </div>
 
-    $query = mysql_real_escape_string($query);
-    // anti-SQL injection
+            </div>
 
-    $raw_results = mysql_query("SELECT * FROM hotels
-      WHERE (`hotelName` LIKE '%".$query."%') OR (`District` LIKE '%".$query."%')") or die(mysql_error());
 
-    if(mysql_num_rows($raw_results) > 0){ // if there are results
+            <?php
+            $query = $_GET['query'];
 
-      echo "<div class='col-sm-8'>
+            $min_length = 3;
+
+            if (strlen($query) >= $min_length) { // if query length > minLen
+                $query = htmlspecialchars($query);
+                // html conversion
+
+                $query = mysql_real_escape_string($query);
+                // anti-SQL injection
+
+                $raw_results = mysql_query("SELECT * FROM hotels
+      WHERE (`hotelName` LIKE '%" . $query . "%') OR (`District` LIKE '%" . $query . "%')") or die(mysql_error());
+
+                if (mysql_num_rows($raw_results) > 0) { // if there are results
+                    echo "<div class='col-sm-8'>
       <table class='table table-hover' style='max-width: none'>
         <thead>
           <tr>
@@ -100,35 +99,31 @@
         </thead>
         <tbody>";
 
-      while($results = mysql_fetch_array($raw_results)){
-      // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+                    while ($results = mysql_fetch_array($raw_results)) {
+                        // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
 
-        echo "            <tr class='hotel_result'>
-              <td id='hotel_name'>".$results['hotelName']."</td>
-              <td id='hotel_type'>".$results['Stars']."</td>
-              <td id='hotel_loc'>".$results['District']."</td>
-              <td id='hotel_daily_pr'>".$results['DailyPrice']."</td>
-              <td id='info_button'><a href='/hotels/".$results['InfoPage']." class='btn btn-default'>Info Page</a></td>
+                        echo "            <tr class='hotel_result'>
+              <td id='hotel_name'>" . $results['hotelName'] . "</td>
+              <td id='hotel_type'>" . $results['Stars'] . "</td>
+              <td id='hotel_loc'>" . $results['District'] . "</td>
+              <td id='hotel_daily_pr'>" . $results['DailyPrice'] . "</td>
+              <td id='info_button'><a href='/hotels/" . $results['InfoPage'] . " class='btn btn-default'>Info Page</a></td>
             </tr>";
-      }
-      echo "          </tbody>
+                    }
+                    echo "          </tbody>
         </table>
     </div>";
-
-    }
-    else{ // if there is no matching rows do following
-      echo "No results";
-    }
-
-  }
-  else{ // if query length is less than minimum
-    echo "Minimum length is ".$min_length;
-  }
-?>
+                } else { // if there is no matching rows do following
+                    echo "No results";
+                }
+            } else { // if query length is less than minimum
+                echo "Minimum length is " . $min_length;
+            }
+            ?>
 
 
-  </div>
-</div>
+        </div>
+    </div>
 
 
 </body>
