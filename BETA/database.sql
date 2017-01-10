21ab8716e6a049eb80cb46997d12cabbcee980f0
Thage <valthonia@gmail.com>
2017-01-10 23:18:06 +0300

    DB Update

--- BETA/database.sql
+++ BETA/database.sql
@@ -5,22 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema HRSdb
+-- Schema hrsdb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `HRSdb` ;
 
 -- -----------------------------------------------------
--- Schema HRSdb
+-- Schema hrsdb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `HRSdb` DEFAULT CHARACTER SET utf8 ;
-USE `HRSdb` ;
+CREATE SCHEMA IF NOT EXISTS `hrsdb` DEFAULT CHARACTER SET utf8 ;
+USE `hrsdb` ;
 
 -- -----------------------------------------------------
--- Table `HRSdb`.`hotels`
+-- Table `hrsdb`.`hotels`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `HRSdb`.`hotels` ;
-
-CREATE TABLE IF NOT EXISTS `HRSdb`.`hotels` (
+CREATE TABLE IF NOT EXISTS `hrsdb`.`hotels` (
   `RegistrationId` INT(11) NOT NULL,
   `HotelName` VARCHAR(45) NOT NULL,
   `District` VARCHAR(20) NOT NULL,
@@ -28,8 +25,8 @@ CREATE TABLE IF NOT EXISTS `HRSdb`.`hotels` (
   `Stars` VARCHAR(1) NOT NULL,
   `DailyPrice` SMALLINT(6) NOT NULL,
   `RoomCount` SMALLINT(6) NOT NULL,
-  `InfoPage` VARCHAR(45) GENERATED ALWAYS AS (CONCAT(RegistrationId,'.html')) VIRTUAL,
-  `Pictures` BLOB NOT NULL,
+  `Pictures` MEDIUMBLOB NULL DEFAULT NULL,
+  `HotelInfo` MEDIUMTEXT NOT NULL,
   PRIMARY KEY (`RegistrationId`),
   UNIQUE INDEX `Registration Code_UNIQUE` (`RegistrationId` ASC))
 ENGINE = InnoDB
@@ -37,15 +34,12 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `HRSdb`.`users`
+-- Table `hrsdb`.`users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `HRSdb`.`users` ;
-
-CREATE TABLE IF NOT EXISTS `HRSdb`.`users` (
+CREATE TABLE IF NOT EXISTS `hrsdb`.`users` (
   `userId` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(90) NOT NULL,
-  `usertype` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
   UNIQUE INDEX `userId_UNIQUE` (`userId` ASC))
@@ -55,11 +49,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `HRSdb`.`hotel users`
+-- Table `hrsdb`.`hotel users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `HRSdb`.`hotel users` ;
-
-CREATE TABLE IF NOT EXISTS `HRSdb`.`hotel users` (
+CREATE TABLE IF NOT EXISTS `hrsdb`.`hotel users` (
   `HotelName` VARCHAR(45) NULL DEFAULT NULL,
   `Hotels_RegistrationId` INT(11) NOT NULL,
   `users_userId` INT(11) NOT NULL,
@@ -68,12 +60,12 @@ CREATE TABLE IF NOT EXISTS `HRSdb`.`hotel users` (
   INDEX `fk_hotel users_users1_idx` (`users_userId` ASC),
   CONSTRAINT `fk_Hotel Users_Hotels1`
     FOREIGN KEY (`Hotels_RegistrationId`)
-    REFERENCES `HRSdb`.`hotels` (`RegistrationId`)
+    REFERENCES `hrsdb`.`hotels` (`RegistrationId`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_hotel users_users1`
     FOREIGN KEY (`users_userId`)
-    REFERENCES `HRSdb`.`users` (`userId`)
+    REFERENCES `hrsdb`.`users` (`userId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -81,11 +73,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `HRSdb`.`reservations`
+-- Table `hrsdb`.`reservations`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `HRSdb`.`reservations` ;
-
-CREATE TABLE IF NOT EXISTS `HRSdb`.`reservations` (
+CREATE TABLE IF NOT EXISTS `hrsdb`.`reservations` (
   `People` TINYINT(4) NOT NULL,
   `RoomAmount` TINYINT(4) NOT NULL,
   `StartDate` DATE NOT NULL,
@@ -98,12 +88,12 @@ CREATE TABLE IF NOT EXISTS `HRSdb`.`reservations` (
   INDEX `fk_reservations_users1_idx` (`users_userId` ASC),
   CONSTRAINT `fk_Reservations_Hotels1`
     FOREIGN KEY (`Hotels_RegistrationId`)
-    REFERENCES `HRSdb`.`hotels` (`RegistrationId`)
+    REFERENCES `hrsdb`.`hotels` (`RegistrationId`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_reservations_users1`
     FOREIGN KEY (`users_userId`)
-    REFERENCES `HRSdb`.`users` (`userId`)
+    REFERENCES `hrsdb`.`users` (`userId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
