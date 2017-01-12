2cc4fd0a610c7478fbe62483de47814380a7a5b4
Serdar Ünlüsoy <serdarunlusoy96@gmail.com>
2017-01-13 02:50:35 +0300

    neccessary after changes on userpage.php

---
+++ BETA/deletereservation.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include_once 'mysqli_connect.php';
+
+if (!isset($_SESSION['usr_id'])) {
+    header("Location: login.php");
+}
+
+$reservationId= $_SESSION['reservationId'];
+
+
+mysqli_query($connection, "DELETE FROM reservations WHERE reservationId = '" . $reservationId . "' ");
+
+?>
